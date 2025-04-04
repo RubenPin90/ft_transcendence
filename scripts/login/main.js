5bb83ebdc49b3ecbe464e00a90ee5ad536586fee
ecaliska42 <enescaliskan1860@gmail.com>
2025-04-04 22:21:58 +0200

    added different buttons for login with google or github

--- scripts/login/main.js
+++ scripts/login/main.js
@@ -1,8 +1,12 @@
 async function toggle_login() {
     const blur = document.getElementById('toggle_login');
     blur.classList.toggle('hidden');
-    const button = document.getElementById('main_button');
-    button.classList.add('hidden');
+    const big_button = document.getElementById('main_button');
+    if (blur.classList.contains('hidden')){
+        big_button.classList.remove('hidden');
+    }else{
+        big_button.classList.add('hidden');
+    }
 }
 
 
@@ -10,6 +14,7 @@ async function login() {
     console.log("HELLO");
     const email = document.getElementById("email-input").value;
     const password = document.getElementById("password-input").value;
+
 
     if (!email || !password) {
         alert("Bitte Email und Passwort eingeben!");
