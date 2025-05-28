53635b7e26d61323cb51a6bb1d058991fc7f03d4
Ruben Pin <pinchasov.ruben@gmail.com>
2025-05-28 22:33:54 +0200

    add_encrypted_pass

---
+++ srcs/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+OAUTH_FILE=./srcs/secrets.yml
+ENV_FILE=./srcs/.env
+USER=$(whoami)
+
+function yaml() {
+    yq -r "$1" "$OAUTH_FILE"
+}
+
+function demo_mode() {
+    echo "Running demo setup..."
+    openssl rand -base64 24
+}
+
+declare -A secrets=(
+    ["google"]=".secrets.google.client_secret"
+    ["github"]=".secrets.github.client_secret"
+    ["smtp"]=".secrets.smtp.pass"
+    ["jwt"]=".secrets.jwt.pass"
+    ["prometheus"]=".secrets.prometheus.pass"
+    ["grafana"]=".secrets.grafana.pass"
+)
+
+declare -A users=(
+    ["GOOGLE_CLIENT_ID"]=".secrets.google.client_id"
+    ["GITHUB_CLIENT_ID"]=".secrets.github.client_id"
+    ["SMTP_USER"]=".secrets.smtp.user"
+)
+
+echo "Setting up environment and secrets..."
+
+if [ -f "$OAUTH_FILE" ]; then
+    for k in ${!secrets[@]}; do 
+        tmp=$(yaml ${secrets[$k]})
+        echo $tmp > ./secrets/${k}.txt;
+    done
+    for k in ${!users[@]}; do
+        sed -i "s|$k=#|$k=$(yaml ${users[$k]})|g" $ENV_FILE;
+    done
+fi
+# else
+#     echo "Generating demo secrets..."
+#     for k in "${!secrets[@]}"; do 
+#         echo "secrets/$k.txt";
+#         demo_mode > "secrets/$k.txt";
+#     done
+#     for k in "${!users[@]}"; do sed -i "s|$k=#|$k=$USER|g" $ENV_FILE; done
